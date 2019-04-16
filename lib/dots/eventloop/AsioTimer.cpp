628d6c9a7f7a755bfa3ac9ba078fb6f589464426
Christopher Gerlach <gerlachch@gmx.com>
2019-04-16 14:23:53 +0200

    apply standard formatting to ASIO timer

--- lib/dots/eventloop/AsioTimer.cpp
+++ lib/dots/eventloop/AsioTimer.cpp
@@ -4,70 +4,67 @@
 
 namespace dots
 {
+	void AsioTimer::onTimeout(const boost::system::error_code& error)
+	{
+		if (error != boost::asio::error::operation_aborted)
+		{
+			if (m_periodic)
+			{
+				m_cb();
+				startAbsolute(m_next += m_interval);
+			}
+			else
+			{
+				m_cb();
+				remTimer(m_id);
+			}
+		}
+	}
 
-void AsioTimer::onTimeout(const boost::system::error_code &error)
-{
-    if (error != boost::asio::error::operation_aborted)
-    {
+	void AsioTimer::startRelative(const pnxs::Duration & duration)
+	{
+		m_timer.expires_from_now(std::chrono::duration_cast<duration_t>(duration));
+		m_timer.async_wait(FUN(*this, onTimeout));
+	}
+
+	void AsioTimer::startAbsolute(const pnxs::SteadyTimePoint & timepoint)
+	{
+		m_timer.expires_at(std::chrono::time_point_cast<duration_t>(timepoint));
+		m_timer.async_wait(FUN(*this, onTimeout));
+	}
+
+	AsioTimer::AsioTimer(const pnxs::Duration & interval, const function<void()> & cb, bool periodic)
+		:m_timer(ioService())
+		, m_cb(cb)
+		, m_id(m_lastTimerId++)
+		, m_interval(interval)
+		, m_next(pnxs::SteadyNow{})
+		, m_periodic(periodic)
+	{
+		s_all[m_id] = this;
+
 		if (m_periodic)
 		{
-			m_cb();
 			startAbsolute(m_next += m_interval);
 		}
 		else
 		{
-			m_cb();
-			remTimer(m_id);
+			startRelative(m_interval);
 		}
-    }
-}
+	}
 
+	AsioTimer::~AsioTimer()
+	{
+		m_timer.cancel();
+		s_all.erase(m_id);
+	}
 
-void AsioTimer::startRelative(const pnxs::Duration &duration)
-{
-    m_timer.expires_from_now(std::chrono::duration_cast<duration_t>(duration));
-    m_timer.async_wait(FUN(*this, onTimeout));
-}
-
-void AsioTimer::startAbsolute(const pnxs::SteadyTimePoint& timepoint)
-{
-    m_timer.expires_at(std::chrono::time_point_cast<duration_t>(timepoint));
-    m_timer.async_wait(FUN(*this, onTimeout));
-}
-
-AsioTimer::AsioTimer(const pnxs::Duration &interval, const function<void()> &cb, bool periodic)
-:m_timer(ioService())
-,m_cb(cb)
-,m_id(m_lastTimerId++)
-,m_interval(interval)
-,m_next(pnxs::SteadyNow{})
-,m_periodic(periodic)
-{
-    s_all[m_id] = this;
-
-    if (m_periodic)
-    {
-        startAbsolute(m_next += m_interval);
-    }
-    else
-    {
-        startRelative(m_interval);
-    }
-}
-
-AsioTimer::~AsioTimer()
-{
-	m_timer.cancel();
-    s_all.erase(m_id);
-}
-
-void AsioTimer::remTimer(unsigned int id)
-{
-    auto iter = s_all.find(id);
-    if (iter != s_all.end())
-    {
-        delete iter->second;
-    }
-}
-
+	void AsioTimer::remTimer(unsigned int id)
+	{
+		auto iter = s_all.find(id);
+		if (iter != s_all.end())
+		{
+			delete iter->second;
+		}
+	}
 }
--- lib/dots/eventloop/AsioTimer.h
+++ lib/dots/eventloop/AsioTimer.h
@@ -7,38 +7,36 @@
 #include <boost/asio.hpp>
 #include <boost/asio/steady_timer.hpp>
 
-namespace dots {
-
-class AsioTimer
+namespace dots
 {
-	using timer_t = boost::asio::steady_timer;
-	using duration_t = timer_t::clock_type::duration;
+	class AsioTimer
+	{
+		using timer_t = boost::asio::steady_timer;
+		using duration_t = timer_t::clock_type::duration;
 
-	timer_t m_timer;
-    const function<void ()> m_cb;
-    unsigned int m_id;
-    pnxs::Duration m_interval;
-    pnxs::SteadyTimePoint m_next;
-    bool m_periodic;
+		timer_t m_timer;
+		const function<void()> m_cb;
+		unsigned int m_id;
+		pnxs::Duration m_interval;
+		pnxs::SteadyTimePoint m_next;
+		bool m_periodic;
 
-	inline static unsigned int m_lastTimerId = 1;
+		inline static unsigned int m_lastTimerId = 1;
 
-    void callCb();
-	void onTimeout(const boost::system::error_code& error);
-
-
-public:
-    AsioTimer(const pnxs::Duration & interval, const function<void ()> &cb, bool periodic = false);
-    ~AsioTimer();
-
-    inline static std::map<unsigned int, AsioTimer*> s_all;
-
-	void startRelative(const pnxs::Duration& duration);
-	void startAbsolute(const pnxs::SteadyTimePoint& timepoint);
-
-    unsigned int id() { return m_id; }
-    static void remTimer(unsigned int id);
-};
+		void callCb();
+		void onTimeout(const boost::system::error_code& error);
 
 
+	public:
+		AsioTimer(const pnxs::Duration& interval, const function<void()>& cb, bool periodic = false);
+		~AsioTimer();
+
+		inline static std::map<unsigned int, AsioTimer*> s_all;
+
+		void startRelative(const pnxs::Duration& duration);
+		void startAbsolute(const pnxs::SteadyTimePoint& timepoint);
+
+		unsigned int id() { return m_id; }
+		static void remTimer(unsigned int id);
+	};
 }
