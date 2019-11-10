032813271b2de99f9de0c3b440a855caf958aa78
Christopher Gerlach <gerlachch@gmx.com>
2019-11-10 17:29:15 +0100

    introduce static descriptor base class
    The descriptors for fundamental (aka base) types are currently provided
    by the StandardTypeDescriptor. This class mostly relays the core
    descriptor operations to the explicit type via casting. Even though the
    general approach is viable, it always completely erases all type
    information. This makes their usage unattractive in situations where
    the type is known and leads to always requiring checks and redundant
    operation code for the static and dynamic case (see descriptor usage in
    the Property class).
    
    The current implementation is also outdated and, among other things,
    does not support construction or assignment with parameters.
    
    This commit introduces a base class template that provides a basic
    descriptor implementation for an arbitrary static type. Contrary to the
    StandardTypeDescriptor, it implements typed versions of the descriptor
    operations and only relays the virtual overrides. This allows the
    operations to be used efficiently without abstraction in cases where
    the type is known, while only requiring one additional virtual
    indirection in the typeless case.
    
    Additionally, it can be used by non-fundamental types as well and also
    centralizes access to one unique descriptor instance, for every static
    type.

---
+++ lib/dots/type/NewStaticDescriptor.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <dots/type/NewDescriptor.h>
+
+namespace dots::type
+{
+	template <typename T, typename Base = NewDescriptor<NewTypeless>>
+	struct NewStaticDescriptor : Base
+	{
+		template <typename Base_ = Base, std::enable_if_t<std::is_same_v<Base_, NewDescriptor<NewTypeless>>, int> = 0>
+		constexpr NewStaticDescriptor(NewType type, std::string name) : Base(type, std::move(name), sizeof(T), alignof(T))
+		{
+			/* do nothing */
+		}
+		
+		template <typename Base_ = Base, typename... Args, std::enable_if_t<!std::is_same_v<Base_, NewDescriptor<NewTypeless>>, int> = 0>
+		constexpr NewStaticDescriptor(Args&&... args) : Base(std::forward<Args>(args)...)
+		{
+			/* do nothing */
+		}
+		
+		NewStaticDescriptor(const NewStaticDescriptor& other) = default;
+		NewStaticDescriptor(NewStaticDescriptor&& other) = default;
+		~NewStaticDescriptor() = default;
+
+		NewStaticDescriptor& operator = (const NewStaticDescriptor& rhs) = default;
+		NewStaticDescriptor& operator = (NewStaticDescriptor&& rhs) = default;
+
+		NewTypeless& construct(NewTypeless& value) const override
+		{
+			return reinterpret_cast<NewTypeless&>(construct(reinterpret_cast<T&>(value)));
+		}
+
+		NewTypeless& construct(NewTypeless& value, const NewTypeless& other) const override
+		{
+			return reinterpret_cast<NewTypeless&>(construct(reinterpret_cast<T&>(value), reinterpret_cast<const T&>(other)));
+		}
+
+		NewTypeless& construct(NewTypeless& value, NewTypeless&& other) const override
+		{
+			return reinterpret_cast<NewTypeless&>(construct(reinterpret_cast<T&>(value), reinterpret_cast<T&&>(other)));
+		}
+
+		template <typename... Args>
+		constexpr T& construct(T& value, Args&&... args) const
+		{
+			static_assert(std::is_constructible_v<T, Args...>, "type is not constructible from passed arguments");
+			if constexpr (std::is_constructible_v<T, Args...>)
+			{
+				::new(static_cast<void*>(::std::addressof(value))) T{ std::forward<Args>(args)... };
+			}
+
+			return value;
+		}
+
+		void destruct(NewTypeless& value) const override
+		{
+			destruct(reinterpret_cast<T&>(value));
+		}
+
+		constexpr void destruct(T& value) const
+		{
+			value.~T();
+		}
+
+		NewTypeless& assign(NewTypeless& lhs, const NewTypeless& rhs) const override
+		{
+			return reinterpret_cast<NewTypeless&>(assign(reinterpret_cast<T&>(lhs), reinterpret_cast<const T&>(rhs)));
+		}
+
+		NewTypeless& assign(NewTypeless& lhs, NewTypeless&& rhs) const override
+		{
+			return reinterpret_cast<NewTypeless&>(assign(reinterpret_cast<T&>(lhs), reinterpret_cast<T&&>(rhs)));
+		}
+
+		template <typename... Args>
+		constexpr T& assign(T& value, Args&&... args) const
+		{
+			static_assert(std::is_constructible_v<T, Args...>, "type is not constructible from passed arguments");
+			if constexpr (std::is_constructible_v<T, Args...>)
+			{
+				value = T{ std::forward<Args>(args)... };
+			}
+
+			return value;
+		}
+
+		void swap(NewTypeless& value, NewTypeless& other) const override
+		{
+			swap(reinterpret_cast<T&>(value), reinterpret_cast<T&>(other));
+		}
+
+		constexpr void swap(T& lhs, T& rhs) const
+		{
+			std::swap(lhs, rhs);
+		}
+
+		bool equal(const NewTypeless& lhs, const NewTypeless& rhs) const override
+		{
+			return equal(reinterpret_cast<const T&>(lhs), reinterpret_cast<const T&>(rhs));
+		}
+
+		constexpr bool equal(const T& lhs, const T& rhs) const
+		{
+			return std::equal_to<T>{}(lhs, rhs);
+		}
+
+		bool less(const NewTypeless& lhs, const NewTypeless& rhs) const override
+		{
+			return less(reinterpret_cast<const T&>(lhs), reinterpret_cast<const T&>(rhs));
+		}
+
+		constexpr bool less(const T& lhs, const T& rhs) const
+		{
+			return std::less<T>{}(lhs, rhs);
+		}
+
+		size_t dynamicMemoryUsage(const NewTypeless& lhs) const override
+		{
+			return dynamicMemoryUsage(reinterpret_cast<const T&>(lhs));
+		}
+
+		constexpr size_t dynamicMemoryUsage(const T&/* lhs*/) const
+		{
+			return 0;
+		}
+
+		static const NewDescriptor<T>& Instance()
+		{
+			static NewDescriptor<T> Instance_;
+			return Instance_;
+		}
+	};
+}
