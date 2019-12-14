6abce04a7a819c0c6d94ac88366b050e62c75b82
Christopher Gerlach <gerlachch@gmx.com>
2019-12-14 17:37:11 +0100

    clean up formatting and dead code in transceiver

--- lib/dots/io/Transceiver.cpp
+++ lib/dots/io/Transceiver.cpp
@@ -44,222 +44,208 @@ namespace dots::type
 namespace dots
 {
 
-Publisher* onPublishObject = nullptr;
+	Publisher* onPublishObject = nullptr;
 
-Transceiver::Transceiver()
-//: m_receiver(FUN(*this, onReceivedMessage))
-{
-    connection().onConnected.connect(FUN(*this, onConnect));
-    connection().onEarlyConnect.connect(FUN(*this, onEarlySubscribe));
-    connection().onReceiveMessage.connect(FUN(m_dispatcher, dispatch));
-
-    onPublishObject = this;
-//    connection().onDisconnected
-}
-
-bool Transceiver::start(const std::string &name, channel_ptr_t channel)
-{
-    LOG_DEBUG_S("start transceiver");
-
-    // start communication
-    if (connection().start(name, channel))
-    {
-       // m_receiver.start(connection().channel());
-        // publish types
-        return true;
-    }
-
-    return false;
-}
-
-void Transceiver::stop()
-{
-    // stop communication
-    connection().stop();
-}
-
-const io::Registry& Transceiver::registry() const
-{
-	return m_registry;	
-}
-
-io::Registry& Transceiver::registry()
-{
-	return m_registry;
-}
-
-const ContainerPool& Transceiver::pool() const
-{
-	return m_dispatcher.pool();
-}
-
-const Container<>& Transceiver::container(const type::StructDescriptor<>& descriptor)
-{
-	return m_dispatcher.container(descriptor);
-}
-
-Subscription Transceiver::subscribe(const type::StructDescriptor<>& descriptor, receive_handler_t<>&& handler)
-{
-	if (descriptor.substructOnly())
+	Transceiver::Transceiver()
 	{
-		throw std::logic_error{ "attempt to subscribe to substruct-only type" };
-	}
-	
-	connection().joinGroup(descriptor.name());
-	return m_dispatcher.subscribe(descriptor, std::move(handler));
-}
+		connection().onConnected.connect(FUN(*this, onConnect));
+		connection().onEarlyConnect.connect(FUN(*this, onEarlySubscribe));
+		connection().onReceiveMessage.connect(FUN(m_dispatcher, dispatch));
 
-Subscription Transceiver::subscribe(const type::StructDescriptor<>& descriptor, event_handler_t<>&& handler)
-{
-	if (descriptor.substructOnly())
-	{
-		throw std::logic_error{ "attempt to subscribe to substruct-only type" };
-	}
-	
-	connection().joinGroup(descriptor.name());
-	return m_dispatcher.subscribe(descriptor, std::move(handler));
-}
-
-Subscription Transceiver::subscribe(const std::string_view& name, receive_handler_t<>&& handler)
-{
-	return subscribe(getDescriptorFromName(name), std::move(handler));
-}
-
-Subscription Transceiver::subscribe(const std::string_view& name, event_handler_t<>&& handler)
-{
-	return subscribe(getDescriptorFromName(name), std::move(handler));
-}
-
-ServerConnection &Transceiver::connection()
-{
-    return m_serverConnection;
-}
-
-
-void Transceiver::publish(const type::StructDescriptor<> *td, const type::Struct& instance, types::property_set_t what, bool remove)
-{
-	if (td->substructOnly())
-	{
-		throw std::logic_error{ "attempt to publish substruct-only type" };
-	}
-	
-    connection().publish(td, instance, what, remove);
-}
-
-void Transceiver::onConnect()
-{
-    m_connected = true;
-}
-
-bool Transceiver::connected() const
-{
-    return m_connected;
-}
-
-void Transceiver::onEarlySubscribe()
-{
-    TD_Traversal traversal;
-
-    for (const auto& td : getDescriptors())
-    {
-        if (td->internal()) continue;
-
-        traversal.traverseDescriptorData(td, [this](auto td, auto body) {
-            this->connection().publishNs("SYS", td, *reinterpret_cast<const type::Struct*>(body), td->validProperties(body), false);
-        });
-    }
-
-    // Send all subscribes
-    for (auto& descriptor: getSubscribedDescriptors())
-    {
-        connection().joinGroup(descriptor->name());
-    }
-
-    // Send preloadClientFinished
-    DotsMsgConnect cm;
-    cm.preloadClientFinished(true);
-
-    connection().publishNs("SYS", &cm._Descriptor(), cm);
-}
-
-type::StructDescriptorSet Transceiver::getPublishedDescriptors() const
-{
-    type::StructDescriptorSet sds;
-
-    for (const auto& e : dots::PublishedType::allChained())
-    {
-        auto td = m_registry.findStructType(e->td->name());
-        if (not td) {
-            throw std::runtime_error("struct decriptor not found for " + e->td->name());
-            //td = type::toStructDescriptor(type::Descriptor<>::registry().registerType(e->t));
-        }
-        if (td) {
-            sds.insert(td.get());
-        } else
-        {
-            LOG_ERROR_S("td is NULL: " << e->td->name())
-        }
-    }
-    return sds;
-}
-
-type::StructDescriptorSet Transceiver::getSubscribedDescriptors() const
-{
-    type::StructDescriptorSet sds;
-
-    for (const auto& e : dots::SubscribedType::allChained())
-    {
-        auto td = m_registry.findStructType(e->td->name());
-        if (not td) {
-            throw std::runtime_error("struct decriptor1 not found for " + e->td->name());
-            //td = type::toStructDescriptor(type::Descriptor<>::registry().registerType(e->t));
-        }
-        if (td) {
-            sds.insert(td.get());
-        } else
-        {
-            LOG_ERROR_S("td is NULL: " << e->td->name());
-        }
-    }
-    return sds;
-}
-
-type::StructDescriptorSet Transceiver::getDescriptors() const
-{
-    type::StructDescriptorSet sds;
-
-    sds.merge(getPublishedDescriptors());
-    sds.merge(getSubscribedDescriptors());
-
-    return sds;
-}
-
-void Transceiver::subscribeDescriptors()
-{
-    //TODO: implement or cleanup?
-    /*
-    dispatcher().addReceiver<EnumDescriptorData>([registry](const EnumDescriptorData::Cbd& cbd) {
-        registry;
-    });
-    dispatcher().addReceiver<StructDescriptorData>();
-*/
-}
-
-const type::StructDescriptor<>& Transceiver::getDescriptorFromName(const std::string_view& name) const
-{
-	const type::Descriptor<>* descriptor = m_registry.findType(name.data()).get();
-
-	if (descriptor == nullptr)
-	{
-		throw std::logic_error{ "could not find a struct type with name: " + std::string{ name.data() } };
+		onPublishObject = this;
 	}
 
-	if (descriptor->type() != type::Type::Struct)
+	bool Transceiver::start(const std::string& name, channel_ptr_t channel)
 	{
-		throw std::logic_error{ "type with name is not a struct type: " + std::string{ name.data() } };
+		LOG_DEBUG_S("start transceiver");
+
+		// start communication
+		if (connection().start(name, channel))
+		{
+			 // publish types
+			return true;
+		}
+
+		return false;
 	}
 
-	return *static_cast<const type::StructDescriptor<>*>(descriptor);
-}
+	void Transceiver::stop()
+	{
+		// stop communication
+		connection().stop();
+	}
+
+	const io::Registry& Transceiver::registry() const
+	{
+		return m_registry;
+	}
+
+	io::Registry& Transceiver::registry()
+	{
+		return m_registry;
+	}
+
+	const ContainerPool& Transceiver::pool() const
+	{
+		return m_dispatcher.pool();
+	}
+
+	const Container<>& Transceiver::container(const type::StructDescriptor<>& descriptor)
+	{
+		return m_dispatcher.container(descriptor);
+	}
+
+	Subscription Transceiver::subscribe(const type::StructDescriptor<>& descriptor, receive_handler_t<>&& handler)
+	{
+		if (descriptor.substructOnly())
+		{
+			throw std::logic_error{ "attempt to subscribe to substruct-only type" };
+		}
+
+		connection().joinGroup(descriptor.name());
+		return m_dispatcher.subscribe(descriptor, std::move(handler));
+	}
+
+	Subscription Transceiver::subscribe(const type::StructDescriptor<>& descriptor, event_handler_t<>&& handler)
+	{
+		if (descriptor.substructOnly())
+		{
+			throw std::logic_error{ "attempt to subscribe to substruct-only type" };
+		}
+
+		connection().joinGroup(descriptor.name());
+		return m_dispatcher.subscribe(descriptor, std::move(handler));
+	}
+
+	Subscription Transceiver::subscribe(const std::string_view& name, receive_handler_t<>&& handler)
+	{
+		return subscribe(getDescriptorFromName(name), std::move(handler));
+	}
+
+	Subscription Transceiver::subscribe(const std::string_view& name, event_handler_t<>&& handler)
+	{
+		return subscribe(getDescriptorFromName(name), std::move(handler));
+	}
+
+	ServerConnection& Transceiver::connection()
+	{
+		return m_serverConnection;
+	}
+
+
+	void Transceiver::publish(const type::StructDescriptor<>* td, const type::Struct& instance, types::property_set_t what, bool remove)
+	{
+		if (td->substructOnly())
+		{
+			throw std::logic_error{ "attempt to publish substruct-only type" };
+		}
+
+		connection().publish(td, instance, what, remove);
+	}
+
+	void Transceiver::onConnect()
+	{
+		m_connected = true;
+	}
+
+	bool Transceiver::connected() const
+	{
+		return m_connected;
+	}
+
+	void Transceiver::onEarlySubscribe()
+	{
+		TD_Traversal traversal;
+
+		for (const auto& td : getDescriptors())
+		{
+			if (td->internal()) continue;
+
+			traversal.traverseDescriptorData(td, [this](auto td, auto body) {
+				this->connection().publishNs("SYS", td, *reinterpret_cast<const type::Struct*>(body), td->validProperties(body), false);
+			});
+		}
+
+		// Send all subscribes
+		for (auto& descriptor : getSubscribedDescriptors())
+		{
+			connection().joinGroup(descriptor->name());
+		}
+
+		// Send preloadClientFinished
+		DotsMsgConnect cm;
+		cm.preloadClientFinished(true);
+
+		connection().publishNs("SYS", &cm._Descriptor(), cm);
+	}
+
+	type::StructDescriptorSet Transceiver::getPublishedDescriptors() const
+	{
+		type::StructDescriptorSet sds;
+
+		for (const auto& e : dots::PublishedType::allChained())
+		{
+			auto td = m_registry.findStructType(e->td->name());
+			if (!td) {
+				throw std::runtime_error("struct decriptor not found for " + e->td->name());
+			}
+			if (td) {
+				sds.insert(td.get());
+			}
+			else
+			{
+				LOG_ERROR_S("td is NULL: " << e->td->name())
+			}
+		}
+		return sds;
+	}
+
+	type::StructDescriptorSet Transceiver::getSubscribedDescriptors() const
+	{
+		type::StructDescriptorSet sds;
+
+		for (const auto& e : dots::SubscribedType::allChained())
+		{
+			auto td = m_registry.findStructType(e->td->name());
+			if (!td) {
+				throw std::runtime_error("struct decriptor1 not found for " + e->td->name());
+			}
+			if (td) {
+				sds.insert(td.get());
+			}
+			else
+			{
+				LOG_ERROR_S("td is NULL: " << e->td->name());
+			}
+		}
+		return sds;
+	}
+
+	type::StructDescriptorSet Transceiver::getDescriptors() const
+	{
+		type::StructDescriptorSet sds;
+
+		sds.merge(getPublishedDescriptors());
+		sds.merge(getSubscribedDescriptors());
+
+		return sds;
+	}
+
+	const type::StructDescriptor<>& Transceiver::getDescriptorFromName(const std::string_view& name) const
+	{
+		const type::Descriptor<>* descriptor = m_registry.findType(name.data()).get();
+
+		if (descriptor == nullptr)
+		{
+			throw std::logic_error{ "could not find a struct type with name: " + std::string{ name.data() } };
+		}
+
+		if (descriptor->type() != type::Type::Struct)
+		{
+			throw std::logic_error{ "type with name is not a struct type: " + std::string{ name.data() } };
+		}
+
+		return *static_cast<const type::StructDescriptor<>*>(descriptor);
+	}
 
 }
--- lib/dots/io/Transceiver.h
+++ lib/dots/io/Transceiver.h
@@ -18,84 +18,76 @@ namespace dots::type
 
 namespace dots
 {
+	extern Publisher* onPublishObject;
 
-extern Publisher* onPublishObject;
-
-class Transceiver: public Publisher
-{
-public:
-
-	template <typename T = type::Struct>
-	using receive_handler_t = Dispatcher::receive_handler_t<T>;
-	template <typename T = type::Struct>
-	using event_handler_t = Dispatcher::event_handler_t<T>;
-
-    Transceiver();
-
-    bool start(const string &name, channel_ptr_t channel);
-    void stop();
-
-	const io::Registry& registry() const;
-	io::Registry& registry();
-
-	const ContainerPool& pool() const;
-	const Container<>& container(const type::StructDescriptor<>& descriptor);
-
-	Subscription subscribe(const type::StructDescriptor<>& descriptor, receive_handler_t<>&& handler);
-    Subscription subscribe(const type::StructDescriptor<>& descriptor, event_handler_t<>&& handler);
-
-	Subscription subscribe(const std::string_view& name, receive_handler_t<>&& handler);
-	Subscription subscribe(const std::string_view& name, event_handler_t<>&& handler);
-
-    ServerConnection& connection();
-
-    type::StructDescriptorSet getPublishedDescriptors() const;
-    type::StructDescriptorSet getSubscribedDescriptors() const;
-    type::StructDescriptorSet getDescriptors() const;
-
-    void subscribeDescriptors();
-
-    bool connected() const;
-
-    void publish(const type::StructDescriptor<>* td, const type::Struct& instance, types::property_set_t what, bool remove) override;
-
-	template <typename T>
-	const Container<T>& container()
+	struct Transceiver : Publisher
 	{
-		return m_dispatcher.container<T>();
-	}
+		template <typename T = type::Struct>
+		using receive_handler_t = Dispatcher::receive_handler_t<T>;
+		template <typename T = type::Struct>
+		using event_handler_t = Dispatcher::event_handler_t<T>;
 
-	template<typename T>
-	Subscription subscribe(receive_handler_t<T>&& handler)
-	{
-		static_assert(!T::_SubstructOnly, "it is not allowed to subscribe to a struct that is marked with 'substruct_only'!");
-		connection().joinGroup(T::_Descriptor().name());
-		return m_dispatcher.subscribe<T>(std::move(handler));
-	}
+		Transceiver();
 
-	template<typename T>
-	Subscription subscribe(event_handler_t<T>&& handler)
-	{
-		static_assert(!T::_SubstructOnly, "it is not allowed to subscribe to a struct that is marked with 'substruct_only'!");
-		connection().joinGroup(T::_Descriptor().name());
+		bool start(const string& name, channel_ptr_t channel);
+		void stop();
 
-		return m_dispatcher.subscribe<T>(std::move(handler));
-	}
+		const io::Registry& registry() const;
+		io::Registry& registry();
 
-private:
-    void onConnect();
-    void onEarlySubscribe();
-	const type::StructDescriptor<>& getDescriptorFromName(const std::string_view& name) const;
+		const ContainerPool& pool() const;
+		const Container<>& container(const type::StructDescriptor<>& descriptor);
 
-    ServerConnection m_serverConnection;
+		Subscription subscribe(const type::StructDescriptor<>& descriptor, receive_handler_t<>&& handler);
+		Subscription subscribe(const type::StructDescriptor<>& descriptor, event_handler_t<>&& handler);
 
-    bool m_connected = false;
+		Subscription subscribe(const std::string_view& name, receive_handler_t<>&& handler);
+		Subscription subscribe(const std::string_view& name, event_handler_t<>&& handler);
 
-	io::Registry m_registry;
-    Dispatcher m_dispatcher;
-    //Receiver m_receiver;
-    //Transmitter m_transmitter;
+		ServerConnection& connection();
 
-};
+		type::StructDescriptorSet getPublishedDescriptors() const;
+		type::StructDescriptorSet getSubscribedDescriptors() const;
+		type::StructDescriptorSet getDescriptors() const;
 
+		bool connected() const;
+
+		void publish(const type::StructDescriptor<>* td, const type::Struct& instance, types::property_set_t what, bool remove) override;
+
+		template <typename T>
+		const Container<T>& container()
+		{
+			return m_dispatcher.container<T>();
+		}
+
+		template<typename T>
+		Subscription subscribe(receive_handler_t<T>&& handler)
+		{
+			static_assert(!T::_SubstructOnly, "it is not allowed to subscribe to a struct that is marked with 'substruct_only'!");
+			connection().joinGroup(T::_Descriptor().name());
+			return m_dispatcher.subscribe<T>(std::move(handler));
+		}
+
+		template<typename T>
+		Subscription subscribe(event_handler_t<T>&& handler)
+		{
+			static_assert(!T::_SubstructOnly, "it is not allowed to subscribe to a struct that is marked with 'substruct_only'!");
+			connection().joinGroup(T::_Descriptor().name());
+
+			return m_dispatcher.subscribe<T>(std::move(handler));
+		}
+
+	private:
+		
+		void onConnect();
+		void onEarlySubscribe();
+		const type::StructDescriptor<>& getDescriptorFromName(const std::string_view& name) const;
+
+		ServerConnection m_serverConnection;
+
+		bool m_connected = false;
+
+		io::Registry m_registry;
+		Dispatcher m_dispatcher;
+	};
 }
