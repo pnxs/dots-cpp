074590d289dfc0267711b61709eb0cfb219732af
Christopher Gerlach <gerlachch@gmx.com>
2020-01-08 20:55:05 +0100

    reorder connection members in header

--- bin/dotsd/Connection.h
+++ bin/dotsd/Connection.h
@@ -22,8 +22,6 @@ namespace dots
     public:
         typedef uint32_t ConnectionId;
         static constexpr ConnectionId ServerId = 1;
-        Connection(const Connection&) = delete;
-        Connection& operator = (const Connection&) = delete;
 
         /*!
          * Create a Connection from a Channel.
@@ -32,6 +30,8 @@ namespace dots
          */
         explicit Connection(channel_ptr_t channel, std::string serverName, ConnectionManager& manager);
         ~Connection();
+        Connection(const Connection&) = delete;
+        Connection& operator = (const Connection&) = delete;
 
         DotsConnectionState state() const;
         const ConnectionId& id() const; ///< return client-id
@@ -45,35 +45,33 @@ namespace dots
          * Directly send a Message to the client.
          * @param msg Message-object
          */
-        void send(const DotsTransportHeader& header, const type::Struct& instance);
-
-        void send(const DotsTransportHeader& header, const Transmission& transmission);
-
         void send(const type::Struct& instance, types::property_set_t includedProperties = types::property_set_t::All, bool remove = false);
+        void send(const DotsTransportHeader& header, const type::Struct& instance);
+        void send(const DotsTransportHeader& header, const Transmission& transmission);
 
         void sendContainerContent(const Container<>& container);
         void sendCacheEnd(const std::string& typeName);
 
     private:
-        void processConnectRequest(const DotsMsgConnect& msg);
-        void processConnectPreloadClientFinished(const DotsMsgConnect& msg);
-        void processMemberMessage(const DotsTransportHeader& header, const DotsMember& member, Connection* connection);
 
         enum class RxTx { rx, tx };
 
-        void logRxTx(RxTx, const DotsTransportHeader& header);
-
-        void onChannelError(const std::exception& e);
         bool onReceivedMessage(const DotsTransportHeader& transportHeader, Transmission&& transmission);
         bool onControlMessage(const DotsTransportHeader& transportHeader, Transmission&& transmission);
         bool onRegularMessage(const DotsTransportHeader& transportHeader, Transmission&& transmission);
 
+        void processConnectRequest(const DotsMsgConnect& msg);
+        void processConnectPreloadClientFinished(const DotsMsgConnect& msg);
+        void processMemberMessage(const DotsTransportHeader& header, const DotsMember& member, Connection* connection);
+
+        void onChannelError(const std::exception& e);
+
+        void logRxTx(RxTx, const DotsTransportHeader& header);
         void setConnectionState(const DotsConnectionState& state);
 
         inline static ConnectionId m_lastConnectionId = ServerId; // 0 is used for unitialized, 1 is used for the server.
 
         dots::Transmitter m_transmitter;
-
         channel_ptr_t m_channel;
         string m_serverName;
         ConnectionManager& m_connectionManager;
