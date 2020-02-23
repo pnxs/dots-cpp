9c20b85786a98fecab83a5305bfd90cd09a37ce7
Christopher Gerlach <gerlachch@gmx.com>
2020-02-23 22:45:20 +0100

    fix recursive merge for static struct types

--- lib/dots/type/StaticStruct.h
+++ lib/dots/type/StaticStruct.h
@@ -159,9 +159,19 @@ namespace dots::type
 			{
 				auto merge = [&](auto& propertyThis, auto& propertyOther)
 				{
-					if (strip_t<decltype(propertyOther)>::IsPartOf(includedProperties) && propertyOther.isValid())
+					using property_t = strip_t<decltype(propertyOther)>;
+					using value_t = typename property_t::value_t;
+
+					if (property_t::IsPartOf(includedProperties) && propertyOther.isValid())
 					{
-						propertyThis = propertyOther;
+						if constexpr (std::is_base_of_v<Struct, value_t>)
+						{
+							propertyThis.constructOrValue()._merge(propertyOther);
+						}
+						else
+						{
+						    propertyThis = propertyOther;
+						}
 					}
 				};
 
