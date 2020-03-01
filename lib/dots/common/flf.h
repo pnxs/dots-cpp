417210cbeb84c3cff9eb80fbb29285bd5db8a8d0
Christopher Gerlach <gerlachch@gmx.com>
2020-03-01 11:49:42 +0100

    clean up FLF helper

--- lib/dots/common/flf.h
+++ lib/dots/common/flf.h
@@ -3,24 +3,29 @@
 
 namespace dots_util
 {
+    /**
+     * File line function.
+     * Stores the information of a specific sourcecode point.
+     */
+    struct Flf
+    {
+        constexpr Flf(const std::string_view& filePath, int l, const std::string_view fun) :
+            file(filePath.substr(filePath.find_last_of('/') + 1)),
+            line(l),
+            func(fun)
+        {}
 
-/**
- * File line function.
- * Stores the information of a specific sourcecode point.
- */
-class Flf
-{
-public:
-    constexpr Flf() = default;
-    constexpr Flf(const std::string_view& filePath, int l, const std::string_view fun): file(filePath.substr(filePath.find_last_of('/') + 1)), line(l), func(fun) {}
+        constexpr Flf(const Flf& other) = default;
+        constexpr Flf(Flf&& other) = default;
+        ~Flf() = default;
 
-    constexpr bool valid() const { return file != nullptr; }
+        Flf& operator = (const Flf& rhs) = default;
+        Flf& operator = (Flf&& rhs) = default;
 
-    std::string_view file;
-    const int line = 0;
-    std::string_view func;
-};
-
+        std::string_view file;
+        const int line;
+        std::string_view func;
+    };
 }
 
 #define FLF dots_util::Flf(__FILE__, __LINE__, __FUNCTION__)
