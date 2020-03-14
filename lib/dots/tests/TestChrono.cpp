ef322c98bd74e9deea0eaa65b0235358c67e51c4
Christopher Gerlach <gerlachch@gmx.com>
2020-03-15 00:56:40 +0100

    remove region dependent timestamp conversion test

--- lib/dots/tests/TestChrono.cpp
+++ lib/dots/tests/TestChrono.cpp
@@ -131,7 +131,6 @@ protected:
 TEST_F(TestTimePoint, toString)
 {
     EXPECT_EQ(TimePoint{ Duration{ UnixTimestampValue } }.toString({}), UnixTimestampString);
-    EXPECT_EQ(TimePoint{ Duration{ UnixTimestampValue } }.toString(TimePoint::ISO8601DateTime, false), UnixTimestampISO8601CET);
     EXPECT_EQ(TimePoint{ Duration{ UnixTimestampValue } }.toString(TimePoint::ISO8601DateTime, true), UnixTimestampISO8601UTC);
 }
 
