53b982c01832ac32f28369fa5e90a4cd1211fad8
Christopher Gerlach <gerlachch@gmx.com>
2020-05-17 16:09:12 +0200

    move shared_ptr_only to tools

--- lib/dots/io/Channel.h
+++ lib/dots/io/Channel.h
@@ -5,6 +5,7 @@
 #include <set>
 #include <dots/io/Transmission.h>
 #include <DotsHeader.dots.h>
+#include <dots/tools/shared_ptr_only.h>
 
 namespace dots::io
 {
@@ -13,51 +14,6 @@ namespace dots::io
 
 namespace dots::io
 {
-    namespace tools
-    {
-		template <typename Derived>
-		struct key
-		{
-            key(const key& other) = default;
-            key(key&& other) = default;
-            ~key() = default;
-
-            key& operator = (const key& rhs) = default;
-            key& operator = (key&& rhs) = default;
-
-		private:
-
-			friend Derived;
-			key() = default;
-		};
-
-        struct shared_ptr_only
-        {
-			using key_t = key<shared_ptr_only>;
-
-            shared_ptr_only(key_t) {};
-            shared_ptr_only(const shared_ptr_only& other) = delete;
-            shared_ptr_only(shared_ptr_only&& other) = delete;
-            ~shared_ptr_only() = default;
-
-            shared_ptr_only& operator = (const shared_ptr_only& rhs) = delete;
-            shared_ptr_only& operator = (shared_ptr_only&& rhs) = delete;
-
-		private:
-
-			template <typename T, typename... Args>
-            friend std::shared_ptr<T> make_shared_ptr_only(Args&&... args);
-
-		    static constexpr key_t Key{};
-        };
-
-		template <typename T, typename... Args>
-	    std::shared_ptr<T> make_shared_ptr_only(Args&&... args)
-	    {
-	        return std::make_shared<T>(shared_ptr_only::Key, std::forward<Args>(args)...);
-	    }
-    }
-
 	struct Channel : tools::shared_ptr_only, std::enable_shared_from_this<Channel>
 	{
 		using key_t = tools::shared_ptr_only::key_t;
