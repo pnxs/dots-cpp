a6acc12f5cb92b565646cca2c9b0243bab5e4ab7
Christopher Gerlach <gerlachch@gmx.com>
2020-12-04 22:55:34 +0100

    avoid negation of unsigned integer type

--- lib/include/dots/type/PropertySet.h
+++ lib/include/dots/type/PropertySet.h
@@ -181,7 +181,7 @@ namespace dots::type
                 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
             };
 
-            return DeBruijnBitMultiplyToPosition[static_cast<index_t>((value & -value) * 0x077CB531u) >> 27];
+            return DeBruijnBitMultiplyToPosition[static_cast<index_t>((value & (~value + 1)) * 0x077CB531u) >> 27];
         }
 
         constexpr index_t lastValid(index_t start = LastProperty) const
