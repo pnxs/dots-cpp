6674667f603255802fad133ecd1c17fe2907114b
Christopher Gerlach <gerlachch@gmx.com>
2021-05-01 19:19:04 +0200

    add Google Test printer extensions for DOTS types
    Since DOTS types are involved in almost all parts of the library in one
    way or another, unit tests involving the library often come down to
    comparing struct instances, properties or other values of DOTS types.
    
    The library should therefore provide corresponding extensions for the
    Google Test printer to make output more readable. To do this, the
    printer is extended by providing an ostream template that prints DOTS
    values by using the default string serialization.
    
    Note that this has to be an ostream operator, because, for unknown
    reasons, a corresponding PrintTo overload is currently not being used
    as expected.

--- lib/include/dots/testing/gtest/gtest.h
+++ lib/include/dots/testing/gtest/gtest.h
@@ -1,3 +1,4 @@
 #pragma once
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <dots/testing/gtest/printers/TypePrinter.h>
---
+++ lib/include/dots/testing/gtest/printers/TypePrinter.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <dots/io/serialization/StringSerializer.h>
+
+namespace dots::type
+{
+    namespace details
+    {
+        template <typename T>
+        constexpr bool is_gtest_printable_v = std::disjunction_v<
+            std::is_same<std::decay_t<T>, uuid_t>,
+            std::is_same<std::decay_t<T>, timepoint_t>,
+            std::is_same<std::decay_t<T>, steady_timepoint_t>,
+            std::is_same<std::decay_t<T>, duration_t>,
+            is_property<std::decay_t<T>>,
+            std::is_base_of<Struct, std::decay_t<T>>
+        >;
+    }
+
+    template <typename T, std::enable_if_t<details::is_gtest_printable_v<T>, int> = 0>
+    std::ostream& operator<<(std::ostream& os, T&& value)
+    {
+        return os << io::to_string(std::forward<T>(value));
+    }
+}
