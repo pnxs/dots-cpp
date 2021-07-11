e133fb7234e9de745236a90ddce53de466c86ac1
Christopher Gerlach <gerlachch@gmx.com>
2021-07-11 14:35:49 +0200

    make transmission printer consistent with matcher

--- lib/include/dots/testing/gtest/printers/TransmissionPrinter.h
+++ lib/include/dots/testing/gtest/printers/TransmissionPrinter.h
@@ -7,6 +7,15 @@ namespace dots::io
 {
     inline void PrintTo(const Transmission& transmission, std::ostream* os)
     {
-        *os << (transmission.header().removeObj == true ? "REMOVE        " : "CREATE/UPDATE ") << to_string(*transmission.instance(), transmission.instance()->_validProperties());
+        if (transmission.header().isFromMyself == true)
+        {
+            *os << (transmission.header().removeObj == true ? "SELF REMOVE        " : "SELF CREATE/UPDATE ");
+        }
+        else
+        {
+            *os << (transmission.header().removeObj == true ? "     REMOVE        " : "     CREATE/UPDATE ");
+        }
+
+        *os << to_string(*transmission.instance(), transmission.instance()->_validProperties());
     }
 }
