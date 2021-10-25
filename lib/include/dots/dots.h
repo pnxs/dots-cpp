6ee3f574c8b1db02e6db61352ef7391db0761a9f
Christopher Gerlach <gerlachch@gmx.com>
2021-10-25 17:42:34 +0200

    merge template definitions with declarations

--- lib/include/dots/dots.h
+++ lib/include/dots/dots.h
@@ -10,11 +10,15 @@
 namespace dots
 {
     Timer::id_t add_timer(type::Duration timeout, std::function<void()> handler, bool periodic = false);
+
     void remove_timer(Timer::id_t id);
 
     #if defined(BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR)
+
     void add_fd_handler(int fileDescriptor, std::function<void()> handler);
+
     void remove_fd_handler(int fileDescriptor);
+
     #endif
 
     #ifndef DOTS_NO_GLOBAL_TRANSCEIVER
@@ -22,25 +26,8 @@ namespace dots
     GuestTransceiver& transceiver(std::string_view name = "dots-transceiver", bool reset = false);
 
     void publish(const type::Struct& instance, std::optional<types::property_set_t> includedProperties = std::nullopt, bool remove = false);
+
     void remove(const type::Struct& instance);
-
-    template<typename T, std::enable_if_t<std::is_base_of_v<type::Struct, T>, int>>
-    void publish(const T& instance, std::optional<types::property_set_t> includedProperties = std::nullopt, bool remove = false);
-
-    template<typename T, std::enable_if_t<std::is_base_of_v<type::Struct, T>, int>>
-    void remove(const T& instance);
-
-    Subscription subscribe(const type::StructDescriptor<>& descriptor, Transceiver::transmission_handler_t handler);
-    Subscription subscribe(const type::StructDescriptor<>& descriptor, Transceiver::event_handler_t<> handler);
-
-    template<typename T, typename EventHandler, typename... Args>
-    Subscription subscribe(EventHandler&& handler, Args&&... args);
-
-    const ContainerPool& pool();
-
-    const Container<>& container(const type::StructDescriptor<>& descriptor);
-    template <typename T>
-    const Container<T>& container();
 
     template<typename T, std::enable_if_t<std::is_base_of_v<type::Struct, T>, int> = 0>
     void publish(const T& instance, std::optional<types::property_set_t> includedProperties/* = std::nullopt*/, bool remove/* = false*/)
@@ -58,12 +45,20 @@ namespace dots
         remove(static_cast<const type::Struct&>(instance));
     }
 
+    Subscription subscribe(const type::StructDescriptor<>& descriptor, Transceiver::transmission_handler_t handler);
+
+    Subscription subscribe(const type::StructDescriptor<>& descriptor, Transceiver::event_handler_t<> handler);
+
     template<typename T, typename EventHandler, typename... Args>
     Subscription subscribe(EventHandler&& handler, Args&&... args)
     {
         io::register_global_subscribe_type<T>();
         return transceiver().subscribe<T>(std::forward<EventHandler>(handler), std::forward<Args>(args)...);
     }
+
+    const ContainerPool& pool();
+
+    const Container<>& container(const type::StructDescriptor<>& descriptor);
 
     template <typename T>
     const Container<T>& container()
