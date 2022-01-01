ce9f12138fc6fd1c1e18199790385861eabdbf4b
Christopher Gerlach <gerlachch@gmx.com>
2022-01-01 16:02:21 +0100

    move handler base to details namespace

--- lib/include/dots/tools/Handler.h
+++ lib/include/dots/tools/Handler.h
@@ -4,195 +4,198 @@
 
 namespace dots::tools
 {
-    /*!
-     * @class HandlerBase Handler.h <dots/tools/Handler.h>
-     *
-     * @brief Base class for all universal handlers.
-     *
-     * @warning This base class is an implementation detail and should not
-     * be used directly.
-     */
-    template <typename R, typename... Args>
-    struct HandlerBase
+    namespace details
     {
-        using return_t = R;
-        using args_t = std::tuple<Args...>;
-
-        template <typename Invocable>
-        static constexpr bool is_compatible_v = std::conjunction_v<
-            std::negation<std::is_same<std::decay_t<Invocable>, HandlerBase>>,
-            std::is_invocable_r<R, std::decay_t<Invocable>, Args...>
-        >;
-
-        template <typename MemFn, typename Obj>
-        static constexpr bool is_compatible_member_function_v = std::conjunction_v<
-            std::is_member_function_pointer<std::decay_t<MemFn>>,
-            std::is_pointer<std::decay_t<Obj>>,
-            std::is_invocable_r<R, std::decay_t<MemFn>, std::decay_t<Obj>, Args...>
-        >;
-        
-        template <typename Invocable, typename... BindArgs>
-        static constexpr bool is_bind_compatible_v = std::conjunction_v<
-            std::negation<std::is_member_function_pointer<std::decay_t<Invocable>>>,
-            std::is_invocable_r<R, std::decay_t<Invocable>, std::decay_t<BindArgs>&..., Args...>
-        >;
-
-        template <typename MemFn, typename Obj, typename... BindArgs>
-        static constexpr bool is_bind_compatible_member_function_v = std::conjunction_v<
-            std::is_member_function_pointer<std::decay_t<MemFn>>,
-            std::is_pointer<std::decay_t<Obj>>,
-            std::is_invocable_r<R, std::decay_t<MemFn>, std::decay_t<Obj>, std::decay_t<BindArgs>&..., Args...>
-        >;
-
         /*!
-         * @brief Construct a new Handler from a specific invocable object.
+         * @class HandlerBase Handler.h <dots/tools/Handler.h>
          *
-         * @tparam Invocable The type of the invocable object to use as a
-         * handler.
+         * @brief Base class for all universal handlers.
          *
-         * @param invocable The object to use as a handler. This can be any
-         * invocable object (e.g a lambda, a global function or a static member
-         * function) that is compatible with the signature \p  R(Args...) .
+         * @warning This base class is an implementation detail and should not
+         * be used directly.
          */
-        template <typename Invocable, std::enable_if_t<is_compatible_v<Invocable>, int> = 0>
-        HandlerBase(Invocable&& invocable) :
-            m_handler{ std::forward<Invocable>(invocable) }
+        template <typename R, typename... Args>
+        struct HandlerBase
         {
-            /* do nothing */
-        }
+            using return_t = R;
+            using args_t = std::tuple<Args...>;
 
-        /*!
-         * @brief Construct a new Handler from a specific member function.
-         *
-         * @tparam MemFn The type of the member function to use as a handler.
-         *
-         * @tparam Obj The class of the member function.
-         *
-         * @param memFn A pointer to the member function to use as a handler.
-         * This can be any function that is compatible with the signature
-         * \p R(Args...) .
-         *
-         * @param obj The object to invoke the member function on.
-         */
-        template <typename MemFn, typename Obj, std::enable_if_t<is_compatible_member_function_v<MemFn, Obj>, int> = 0>
-        HandlerBase(MemFn&& memFn, Obj&& obj) :
-            m_handler{ wrapInvocable(std::forward<MemFn>(memFn), std::forward<Obj>(obj)) }
-        {
-            /* do nothing */
-        }
+            template <typename Invocable>
+            static constexpr bool is_compatible_v = std::conjunction_v<
+                std::negation<std::is_same<std::decay_t<Invocable>, HandlerBase>>,
+                std::is_invocable_r<R, std::decay_t<Invocable>, Args...>
+            >;
 
-        /*!
-         * @brief Construct a new Handler from a specific invocable object with
-         * bound arguments.
-         *
-         * @tparam Invocable The type of the invocable object to use as a
-         * handler.
-         *
-         * @tparam BindArgs The types of the arguments to bind to the handler.
-         *
-         * @param invocable The object to use as a handler. This can be any
-         * invocable object (e.g a lambda, a global function or a static member
-         * function) that is compatible with the signature
-         * \p R(BindArgs&...,Args...) .
-         *
-         * @param bindArgs The arguments to bind and pass as references when
-         * the handler is invoked.
-         */
-        template <typename Invocable, typename... BindArgs, std::enable_if_t<is_bind_compatible_v<Invocable, BindArgs...>, int> = 0>
-        HandlerBase(Invocable&& invocable, BindArgs&&... bindArgs) :
-            m_handler{ wrapInvocable(std::forward<Invocable>(invocable), std::forward<BindArgs>(bindArgs)...) }
-        {
-            /* do nothing */
-        }
+            template <typename MemFn, typename Obj>
+            static constexpr bool is_compatible_member_function_v = std::conjunction_v<
+                std::is_member_function_pointer<std::decay_t<MemFn>>,
+                std::is_pointer<std::decay_t<Obj>>,
+                std::is_invocable_r<R, std::decay_t<MemFn>, std::decay_t<Obj>, Args...>
+            >;
+            
+            template <typename Invocable, typename... BindArgs>
+            static constexpr bool is_bind_compatible_v = std::conjunction_v<
+                std::negation<std::is_member_function_pointer<std::decay_t<Invocable>>>,
+                std::is_invocable_r<R, std::decay_t<Invocable>, std::decay_t<BindArgs>&..., Args...>
+            >;
 
-        /*!
-         * @brief Construct a new Handler from a specific member function with
-         * bound arguments.
-         *
-         * @tparam MemFn The type of the member function to use as a handler.
-         *
-         * @tparam Obj The class of the member function.
-         *
-         * @tparam BindArgs The types of the arguments to bind to the handler.
-         *
-         * @param memFn A pointer to the member function to use as a handler.
-         * This can be any function that is compatible with the signature
-         * \p R(BindArgs&...,Args...) .
-         *
-         * @param obj The object to invoke the member function on.
-         *
-         * @param bindArgs The arguments to bind and pass as references when
-         * the handler is invoked.
-         */
-        template <typename MemFn, typename Obj, typename... BindArgs, std::enable_if_t<is_bind_compatible_member_function_v<MemFn, Obj, BindArgs...>, int> = 0>
-        HandlerBase(MemFn&& memFn, Obj&& obj, BindArgs&&... bindArgs) :
-            m_handler{ wrapInvocable(std::forward<MemFn>(memFn), std::forward<Obj>(obj), std::forward<BindArgs>(bindArgs)...) }
-        {
-            /* do nothing */
-        }
+            template <typename MemFn, typename Obj, typename... BindArgs>
+            static constexpr bool is_bind_compatible_member_function_v = std::conjunction_v<
+                std::is_member_function_pointer<std::decay_t<MemFn>>,
+                std::is_pointer<std::decay_t<Obj>>,
+                std::is_invocable_r<R, std::decay_t<MemFn>, std::decay_t<Obj>, std::decay_t<BindArgs>&..., Args...>
+            >;
 
-        HandlerBase(const HandlerBase& other) = default;
-        HandlerBase(HandlerBase&& other) = default;
-        ~HandlerBase() = default;
-
-        HandlerBase& operator = (const HandlerBase& rhs) = default;
-        HandlerBase& operator = (HandlerBase&& rhs) = default;
-
-        /*!
-         * @brief Invoke the handler.
-         *
-         * This calls the underlying invocable object with the given arguments.
-         *
-         * @param args The arguments to call the invocable object with.
-         *
-         * @return R The return value of the call.
-         */
-        R operator () (Args... args) const
-        {
-            return m_handler(std::forward<Args>(args)...);
-        }
-
-    protected:
-
-        HandlerBase() = default;
-
-        std::function<R(Args...)> m_handler;
-
-    private:
-
-        template <typename Invocable, typename... BindArgs>
-        using is_const_wrappable = std::is_invocable_r<R, std::decay_t<Invocable>, const std::decay_t<BindArgs>&..., Args...>;
-
-        template <typename Invocable, typename... BindArgs>
-        using is_mutable_wrappable = std::conjunction<
-            std::negation<is_const_wrappable<Invocable, BindArgs...>>,
-            std::is_invocable_r<R, std::decay_t<Invocable>, std::decay_t<BindArgs>&..., Args...>
-        >;
-
-        template <typename Invocable, typename... BindArgs, std::enable_if_t<is_const_wrappable<Invocable, BindArgs...>::value, int> = 0>
-        auto wrapInvocable(Invocable&& invocable, BindArgs&&... bindArgs)
-        {
-            return [invocable{ std::forward<Invocable>(invocable) }, bindTuple = std::make_tuple(std::forward<BindArgs>(bindArgs)...)](Args... args) -> R
+            /*!
+             * @brief Construct a new Handler from a specific invocable object.
+             *
+             * @tparam Invocable The type of the invocable object to use as a
+             * handler.
+             *
+             * @param invocable The object to use as a handler. This can be any
+             * invocable object (e.g a lambda, a global function or a static member
+             * function) that is compatible with the signature \p  R(Args...) .
+             */
+            template <typename Invocable, std::enable_if_t<is_compatible_v<Invocable>, int> = 0>
+            HandlerBase(Invocable&& invocable) :
+                m_handler{ std::forward<Invocable>(invocable) }
             {
-                return std::apply([&](auto&&... bindArgs)
-                {
-                    return std::invoke(invocable, std::forward<decltype(bindArgs)>(bindArgs)..., std::forward<Args>(args)...);
-                }, bindTuple);
-            };
-        }
+                /* do nothing */
+            }
 
-        template <typename Invocable, typename... BindArgs, std::enable_if_t<is_mutable_wrappable<Invocable, BindArgs...>::value, int> = 0>
-        auto wrapInvocable(Invocable&& invocable, BindArgs&&... bindArgs)
-        {
-            return [invocable{ std::forward<Invocable>(invocable) }, bindTuple = std::make_tuple(std::forward<BindArgs>(bindArgs)...)](Args... args) mutable -> R
+            /*!
+             * @brief Construct a new Handler from a specific member function.
+             *
+             * @tparam MemFn The type of the member function to use as a handler.
+             *
+             * @tparam Obj The class of the member function.
+             *
+             * @param memFn A pointer to the member function to use as a handler.
+             * This can be any function that is compatible with the signature
+             * \p R(Args...) .
+             *
+             * @param obj The object to invoke the member function on.
+             */
+            template <typename MemFn, typename Obj, std::enable_if_t<is_compatible_member_function_v<MemFn, Obj>, int> = 0>
+            HandlerBase(MemFn&& memFn, Obj&& obj) :
+                m_handler{ wrapInvocable(std::forward<MemFn>(memFn), std::forward<Obj>(obj)) }
             {
-                return std::apply([&](auto&&... bindArgs)
+                /* do nothing */
+            }
+
+            /*!
+             * @brief Construct a new Handler from a specific invocable object with
+             * bound arguments.
+             *
+             * @tparam Invocable The type of the invocable object to use as a
+             * handler.
+             *
+             * @tparam BindArgs The types of the arguments to bind to the handler.
+             *
+             * @param invocable The object to use as a handler. This can be any
+             * invocable object (e.g a lambda, a global function or a static member
+             * function) that is compatible with the signature
+             * \p R(BindArgs&...,Args...) .
+             *
+             * @param bindArgs The arguments to bind and pass as references when
+             * the handler is invoked.
+             */
+            template <typename Invocable, typename... BindArgs, std::enable_if_t<is_bind_compatible_v<Invocable, BindArgs...>, int> = 0>
+            HandlerBase(Invocable&& invocable, BindArgs&&... bindArgs) :
+                m_handler{ wrapInvocable(std::forward<Invocable>(invocable), std::forward<BindArgs>(bindArgs)...) }
+            {
+                /* do nothing */
+            }
+
+            /*!
+             * @brief Construct a new Handler from a specific member function with
+             * bound arguments.
+             *
+             * @tparam MemFn The type of the member function to use as a handler.
+             *
+             * @tparam Obj The class of the member function.
+             *
+             * @tparam BindArgs The types of the arguments to bind to the handler.
+             *
+             * @param memFn A pointer to the member function to use as a handler.
+             * This can be any function that is compatible with the signature
+             * \p R(BindArgs&...,Args...) .
+             *
+             * @param obj The object to invoke the member function on.
+             *
+             * @param bindArgs The arguments to bind and pass as references when
+             * the handler is invoked.
+             */
+            template <typename MemFn, typename Obj, typename... BindArgs, std::enable_if_t<is_bind_compatible_member_function_v<MemFn, Obj, BindArgs...>, int> = 0>
+            HandlerBase(MemFn&& memFn, Obj&& obj, BindArgs&&... bindArgs) :
+                m_handler{ wrapInvocable(std::forward<MemFn>(memFn), std::forward<Obj>(obj), std::forward<BindArgs>(bindArgs)...) }
+            {
+                /* do nothing */
+            }
+
+            HandlerBase(const HandlerBase& other) = default;
+            HandlerBase(HandlerBase&& other) = default;
+            ~HandlerBase() = default;
+
+            HandlerBase& operator = (const HandlerBase& rhs) = default;
+            HandlerBase& operator = (HandlerBase&& rhs) = default;
+
+            /*!
+             * @brief Invoke the handler.
+             *
+             * This calls the underlying invocable object with the given arguments.
+             *
+             * @param args The arguments to call the invocable object with.
+             *
+             * @return R The return value of the call.
+             */
+            R operator () (Args... args) const
+            {
+                return m_handler(std::forward<Args>(args)...);
+            }
+
+        protected:
+
+            HandlerBase() = default;
+
+            std::function<R(Args...)> m_handler;
+
+        private:
+
+            template <typename Invocable, typename... BindArgs>
+            using is_const_wrappable = std::is_invocable_r<R, std::decay_t<Invocable>, const std::decay_t<BindArgs>&..., Args...>;
+
+            template <typename Invocable, typename... BindArgs>
+            using is_mutable_wrappable = std::conjunction<
+                std::negation<is_const_wrappable<Invocable, BindArgs...>>,
+                std::is_invocable_r<R, std::decay_t<Invocable>, std::decay_t<BindArgs>&..., Args...>
+            >;
+
+            template <typename Invocable, typename... BindArgs, std::enable_if_t<is_const_wrappable<Invocable, BindArgs...>::value, int> = 0>
+            auto wrapInvocable(Invocable&& invocable, BindArgs&&... bindArgs)
+            {
+                return [invocable{ std::forward<Invocable>(invocable) }, bindTuple = std::make_tuple(std::forward<BindArgs>(bindArgs)...)](Args... args) -> R
                 {
-                    return std::invoke(invocable, std::forward<decltype(bindArgs)>(bindArgs)..., std::forward<Args>(args)...);
-                }, bindTuple);
-            };
-        }
-    };
+                    return std::apply([&](auto&&... bindArgs)
+                    {
+                        return std::invoke(invocable, std::forward<decltype(bindArgs)>(bindArgs)..., std::forward<Args>(args)...);
+                    }, bindTuple);
+                };
+            }
+
+            template <typename Invocable, typename... BindArgs, std::enable_if_t<is_mutable_wrappable<Invocable, BindArgs...>::value, int> = 0>
+            auto wrapInvocable(Invocable&& invocable, BindArgs&&... bindArgs)
+            {
+                return [invocable{ std::forward<Invocable>(invocable) }, bindTuple = std::make_tuple(std::forward<BindArgs>(bindArgs)...)](Args... args) mutable -> R
+                {
+                    return std::apply([&](auto&&... bindArgs)
+                    {
+                        return std::invoke(invocable, std::forward<decltype(bindArgs)>(bindArgs)..., std::forward<Args>(args)...);
+                    }, bindTuple);
+                };
+            }
+        };
+    }
 
     template <typename T>
     struct Handler
@@ -217,9 +220,9 @@ namespace dots::tools
      * "nullability" is required.
      */
     template <typename R>
-    struct Handler<R()> : HandlerBase<R>
+    struct Handler<R()> : details::HandlerBase<R>
     {
-        using HandlerBase<R>::HandlerBase;
+        using details::HandlerBase<R>::HandlerBase;
     };
 
     struct static_argument_cast_tag{};
@@ -242,7 +245,7 @@ namespace dots::tools
      * "nullability" is required.
      */
     template <typename R, typename Arg>
-    struct Handler<R(Arg)> : HandlerBase<R, Arg>
+    struct Handler<R(Arg)> : details::HandlerBase<R, Arg>
     {
         template <typename ArgOther>
         using is_compatible_by_argument = std::conjunction<
@@ -250,7 +253,7 @@ namespace dots::tools
             std::is_base_of<std::decay_t<Arg>, std::decay_t<ArgOther>>
         >;
 
-        using HandlerBase<R, Arg>::HandlerBase;
+        using details::HandlerBase<R, Arg>::HandlerBase;
 
         /*!
          * @brief Construct a new Handler by wrapping another compatible
@@ -280,7 +283,7 @@ namespace dots::tools
         Handler(static_argument_cast_tag tag, Handler<R(ArgOther)>&& other)
         {
             (void)tag;
-            HandlerBase<R, Arg>::m_handler = [handler{ std::move(other.m_handler)}](Arg arg) -> R
+            details::HandlerBase<R, Arg>::m_handler = [handler{ std::move(other.m_handler)}](Arg arg) -> R
             {
                 return std::invoke(handler, static_cast<ArgOther>(arg));
             };
@@ -309,9 +312,9 @@ namespace dots::tools
      * "nullability" is required.
      */
     template <typename R, typename... Args>
-    struct Handler<R(Args...)> : HandlerBase<R, Args...>
+    struct Handler<R(Args...)> : details::HandlerBase<R, Args...>
     {
-        using HandlerBase<R, Args...>::HandlerBase;
+        using details::HandlerBase<R, Args...>::HandlerBase;
     };
 
     namespace details
