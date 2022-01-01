15db4b573b956acc2a290cde97d40f81dfba131a
Christopher Gerlach <gerlachch@gmx.com>
2022-01-01 15:50:18 +0100

    document universal handler interface

--- lib/include/dots/tools/Handler.h
+++ lib/include/dots/tools/Handler.h
@@ -4,6 +4,14 @@
 
 namespace dots::tools
 {
+    /*!
+     * @class HandlerBase Handler.h <dots/tools/Handler.h>
+     *
+     * @brief Base class for all universal handlers.
+     *
+     * @warning This base class is an implementation detail and should not
+     * be used directly.
+     */
     template <typename R, typename... Args>
     struct HandlerBase
     {
@@ -29,6 +37,16 @@ namespace dots::tools
             std::is_invocable_r<R, std::decay_t<MemFn>, std::decay_t<Obj>, std::decay_t<BindArgs>&..., Args...>
         >;
 
+        /*!
+         * @brief Construct a new Handler from a specific invocable object.
+         *
+         * @tparam Invocable The type of the invocable object to use as a
+         * handler.
+         *
+         * @param invocable The object to use as a handler. This can be any
+         * invocable object (e.g a lambda, a global function or a static member
+         * function) that is compatible with the signature \p  R(Args...) .
+         */
         template <typename Invocable, std::enable_if_t<is_compatible_v<Invocable>, int> = 0>
         HandlerBase(Invocable&& invocable) :
             m_handler{ std::forward<Invocable>(invocable) }
@@ -36,6 +54,23 @@ namespace dots::tools
             /* do nothing */
         }
 
+        /*!
+         * @brief Construct a new Handler from a specific invocable object with
+         * bound arguments.
+         *
+         * @tparam Invocable The type of the invocable object to use as a
+         * handler.
+         *
+         * @tparam BindArgs The types of the arguments to bind to the handler.
+         *
+         * @param invocable The object to use as a handler. This can be any
+         * invocable object (e.g a lambda, a global function or a static member
+         * function) that is compatible with the signature
+         * \p R(BindArgs&...,Args...) .
+         *
+         * @param bindArgs The arguments to bind and pass as references when
+         * the handler is invoked.
+         */
         template <typename Invocable, typename... BindArgs, std::enable_if_t<is_bind_compatible_v<Invocable, BindArgs...>, int> = 0>
         HandlerBase(Invocable&& invocable, BindArgs&&... bindArgs) :
             m_handler{ wrapInvocable(std::forward<Invocable>(invocable), std::forward<BindArgs>(bindArgs)...) }
@@ -43,6 +78,25 @@ namespace dots::tools
             /* do nothing */
         }
 
+        /*!
+         * @brief Construct a new Handler from a specific member function with
+         * bound arguments.
+         *
+         * @tparam MemFn The type of the member function to use as a handler.
+         *
+         * @tparam Obj The class of the member function.
+         *
+         * @tparam BindArgs The types of the arguments to bind to the handler.
+         *
+         * @param memFn A pointer to the member function to use as a handler.
+         * This can be any function that is compatible with the signature
+         * \p R(BindArgs&...,Args...) .
+         *
+         * @param obj The object to invoke the member function on.
+         *
+         * @param bindArgs The arguments to bind and pass as references when
+         * the handler is invoked.
+         */
         template <typename MemFn, typename Obj, typename... BindArgs, std::enable_if_t<is_bind_compatible_member_function_v<MemFn, Obj, BindArgs...>, int> = 0>
         HandlerBase(MemFn&& memFn, Obj&& obj, BindArgs&&... bindArgs) :
             m_handler{ wrapInvocable(std::forward<MemFn>(memFn), std::forward<Obj>(obj), std::forward<BindArgs>(bindArgs)...) }
@@ -57,6 +111,15 @@ namespace dots::tools
         HandlerBase& operator = (const HandlerBase& rhs) = default;
         HandlerBase& operator = (HandlerBase&& rhs) = default;
 
+        /*!
+         * @brief Invoke the handler.
+         *
+         * This calls the underlying invocable object with the given arguments.
+         *
+         * @param args The arguments to call the invocable object with.
+         *
+         * @return R The return value of the call.
+         */
         R operator () (Args... args) const
         {
             return m_handler(std::forward<Args>(args)...);
@@ -110,6 +173,22 @@ namespace dots::tools
         static_assert(std::negation_v<std::is_same<T, T>>, "T has to be a function type");
     };
 
+    /*!
+     * @class Handler Handler.h <dots/tools/Handler.h>
+     *
+     * @brief Specialization for handlers without parameters.
+     *
+     * The Handler class can be used to store user-provided invocable
+     * objects and use them as handlers.
+     *
+     * It provides similar functionality to std::function with additional
+     * convenience functionality for member functions and argument binding.
+     *
+     * @remark Contrary to std::function, a Handler always contains a valid
+     * invocable object and is therefore not default-constructible.
+     * Instead, users are advised to use an std::optional when
+     * "nullability" is required.
+     */
     template <typename R>
     struct Handler<R()> : HandlerBase<R>
     {
@@ -119,6 +198,22 @@ namespace dots::tools
     struct static_argument_cast_tag{};
     constexpr static_argument_cast_tag static_argument_cast;
 
+    /*!
+     * @class Handler Handler.h <dots/tools/Handler.h>
+     *
+     * @brief Specialization for handlers with a single parameter.
+     *
+     * The Handler class can be used to store user-provided invocable
+     * objects and use them as handlers.
+     *
+     * It provides similar functionality to std::function with additional
+     * convenience functionality for member functions and argument binding.
+     *
+     * @remark Contrary to std::function, a Handler always contains a valid
+     * invocable object and is therefore not default-constructible.
+     * Instead, users are advised to use an std::optional when
+     * "nullability" is required.
+     */
     template <typename R, typename Arg>
     struct Handler<R(Arg)> : HandlerBase<R, Arg>
     {
@@ -130,9 +225,34 @@ namespace dots::tools
 
         using HandlerBase<R, Arg>::HandlerBase;
 
+        /*!
+         * @brief Construct a new Handler by wrapping another compatible
+         * handler and statically downcasting the argument.
+         *
+         * This constructs a Handler of signature \p R(Arg) from another
+         * handler with signature \p R(ArgOther), where \p Arg is a base class
+         * of \p ArgOther.
+         *
+         * When the handler is invoked, the argument will statically be
+         * downcasted from \p Arg to \p ArgOther.
+         *
+         * @warning The implementation only enforces that type \p Arg is
+         * statically downcastable to \p ArgOther. There are no runtime checks
+         * that a particular argument is actually of that type. It is the
+         * user's responsibility to ensure that the handler is only invoked
+         * with appropriate arguments.
+         *
+         * @tparam ArgOther The argument to statically downcast to. Must be a
+         * subclass of \p Arg .
+         *
+         * @param tag Disambiguation tag for static argument casts.
+         *
+         * @param other The handler object to wrap.
+         */
         template <typename ArgOther, std::enable_if_t<is_compatible_by_argument<ArgOther>::value, int> = 0>
-        Handler(static_argument_cast_tag, Handler<R(ArgOther)>&& other)
+        Handler(static_argument_cast_tag tag, Handler<R(ArgOther)>&& other)
         {
+            (void)tag;
             HandlerBase<R, Arg>::m_handler = [handler{ std::move(other.m_handler)}](Arg arg) -> R
             {
                 return std::invoke(handler, static_cast<ArgOther>(arg));
@@ -145,6 +265,22 @@ namespace dots::tools
         friend struct Handler;
     };
 
+    /*!
+     * @class Handler Handler.h <dots/tools/Handler.h>
+     *
+     * @brief Specialization for handlers with on or more parameters.
+     *
+     * The Handler class can be used to store user-provided invocable
+     * objects and use them as handlers.
+     *
+     * It provides similar functionality to std::function with additional
+     * convenience functionality for member functions and argument binding.
+     *
+     * @remark Contrary to std::function, a Handler always contains a valid
+     * invocable object and is therefore not default-constructible.
+     * Instead, users are advised to use an std::optional when
+     * "nullability" is required.
+     */
     template <typename R, typename... Args>
     struct Handler<R(Args...)> : HandlerBase<R, Args...>
     {
