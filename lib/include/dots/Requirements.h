03481bfe278e9e58cbd4af96fd3344010ab555b4
Christopher Gerlach <gerlachch@gmx.com>
2022-10-13 17:16:20 +0200

    safeguard required C++ features
    Since compilers more and more move in the direction of supporting many,
    but not all features of a standard (particularly since C++20, see [1]),
    it becomes challenging to ensure that a certain set of compilers is
    supported.
    
    Furthermore, users might get confusing build errors when using a
    compiler that supports the version configured via CMake (e.g. C++20),
    but is missing a required feature.
    
    For this reason, the library should use features selectively and have a
    mechanism that conveys clear errors to the user when the compiler is
    incompatible. Unfortunately this cannot be done via CMake, which
    discontinued support for handling particular low level C++ features
    since C++17 (see [2]).
    
    Another way to achieve this is to use feature testing macros, which
    were introduced in C++20 (see [3]).
    
    References:
    
     - [1] https://en.cppreference.com/w/cpp/compiler_support
    
     - [2] https://cmake.org/cmake/help/v3.20/prop_gbl/CMAKE_CXX_KNOWN_FEATURES.html#low-level-individual-compile-features
    
     - [3] https://en.cppreference.com/w/cpp/feature_test

---
+++ lib/include/dots/Requirements.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <version>
+
+/*!
+ * @file Requirements.h
+ *
+ * @brief ISO C++ requirements for dots-cpp.
+ *
+ * This header safeguards all C++20 features or later that are required
+ * to build and use this library.
+ *
+ * If you are an author and want to use a C++ language feature in the
+ * library that is not already listed here, ensure first that it is
+ * provided (see [1]) by the desired set of officially supported
+ * compilers specified in the README. Then add a preprocessor directive
+ * based on the corresponding feature test macro (see [2]) according to
+ * the example below.
+ *
+ * @code{.cpp}
+ * #if not defined(__cpp_concepts)
+ * #error "compiler is missing required feature: __cpp_concepts"
+ * #endif
+ * @endcode
+ *
+ * References:
+ *
+ *  - [1] https://en.cppreference.com/w/cpp/compiler_support
+ *
+ *  - [2] https://en.cppreference.com/w/cpp/feature_test
+ */
